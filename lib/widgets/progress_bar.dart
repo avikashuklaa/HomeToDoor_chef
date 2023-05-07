57a9f21cbcdb955ba00681ec24ebfe8bb3881456
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-05-07 23:29:04 +0530

    Add files via upload

---
+++ lib/widgets/progress_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+circularProgress() {
+  return Container(
+    alignment: Alignment.center,
+    padding: EdgeInsets.only(top: 12),
+    child: CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(
+        Colors.grey,
+      ),
+    ),
+  );
+}
+
+linearProgress() {
+  return Container(
+    alignment: Alignment.center,
+    padding: EdgeInsets.only(top: 12),
+    child: LinearProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(
+        Colors.grey,
+      ),
+    ),
+  );
+}
